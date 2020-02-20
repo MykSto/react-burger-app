487a3b6ca3cda991009f397383d48c9fa9fb64e9
Michail <hushokas@gmail.com>
2020-02-20 09:23:39 +0200

    []: refactoring

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import Aux from 'hoc/Auxiliary';
 import Backdrop from 'components/UI/Backdrop/Backdrop';
 import styles from './Modal.module.css';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop isOrdered={props.isOrdered} clicked={props.modalClosed} />
-    <div
-      className={styles.Modal}
-      style={{
-        transform: props.isOrdered ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.isOrdered ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.isOrdered !== this.props.isOrdered;
+  }
 
-export default modal;
+  UNSAFE_componentWillUpdate() {
+    console.log('[Modal] will update');
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop isOrdered={this.props.isOrdered} clicked={this.props.modalClosed} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.isOrdered ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.isOrdered ? '1' : '0',
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Modal;
