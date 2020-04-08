5afa8fcae683888142cf693ec5468148f5e177c0
Michail <hushokas@gmail.com>
2020-04-08 17:49:37 +0300

    [024]add: protected Data, orders by spec userId

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as authAction from 'store/actions/index';
 
@@ -19,20 +19,36 @@ function App(props) {
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
-        </Switch>
+        { props.token
+          ? (
+            <Switch>
+              <Route path="/logout" component={Logout} />
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/orders" component={Orders} />
+              <Route path="/" exact component={BurgerBuilder} />
+              {/* <Redirect path="/" /> */}
+            </Switch>
+          )
+
+          : (
+            <Switch>
+              <Route path="/auth" component={Auth} />
+              <Route path="/" exact component={BurgerBuilder} />
+              {/* <Redirect path="/" /> */}
+            </Switch>
+          )}
+
       </Layout>
     </div>
   );
 }
 
+const mapStateToProps = (state) => ({
+  token: state.auth.token !== null,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   onLocalStorageToken: () => dispatch(authAction.authCheckState()),
 });
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
